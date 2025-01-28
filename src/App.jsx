0cf481094945492ad465b50ac9883469d93dbe45
Fieraol <fierashiferaw@gmail.com>
2025-01-28 16:17:59 -0500

    contact section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Contact from "./components/Contact/Contact";
 import Header from "./components/Header/Header";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Projects />
       <Skills />
+      <Contact />
     </>
   );
 }
