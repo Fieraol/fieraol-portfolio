0d28603c993a7eb926b5d631a015ae5bb5e60d47
Fieraol <fierashiferaw@gmail.com>
2025-01-28 16:24:01 -0500

    footer section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Projects from "./components/Projects/Projects";
 import Skills from "./components/Skills/Skills";
@@ -11,6 +12,7 @@ function App() {
       <Projects />
       <Skills />
       <Contact />
+      <Footer />
     </>
   );
 }
