f3314a514c7ab4c114bfba3c5f36e7c1b950491c
Fieraol <fierashiferaw@gmail.com>
2025-01-28 14:35:15 -0500

    Created all project cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,8 @@
 import styles from "./ProjectsStyles.module.css";
 import statPaddr from "../../assets/statpaddr.png";
+import librarydb from "../../assets/librarydb.png";
+import clearcellgame from "../../assets/clearcellgame.png";
+import portfolio from "../../assets/portfolio.png";
 import ProjectCard from "../../common/ProjectCard";
 
 function Projects() {
@@ -14,10 +17,22 @@ function Projects() {
           p="Live NBA Standings App"
         />
         <ProjectCard
-          src={statPaddr}
-          link="https://github.com/Fieraol/Final_Project_Test"
-          h3="StatPaddr"
-          p="Live NBA Standings App"
+          src={librarydb}
+          link="https://github.com/Fieraol/library-database"
+          h3="Library Database"
+          p="Database of books with additional info using SQL"
+        />
+        <ProjectCard
+          src={clearcellgame}
+          link="https://github.com/Fieraol/ClearCellGame"
+          h3="Clear Cell Game"
+          p="A Java based game where the objective is to maximize the colored cells cleared from the board"
+        />
+        <ProjectCard
+          src={portfolio}
+          link="https://github.com/Fieraol/fieraol-portfolio"
+          h3="Portfolio"
+          p="My portfolio created using React.js"
         />
       </div>
     </section>
